89f949e8e6f1583dff774fe708661168b8d29d32
eugengnc <eni@njit.edu>
2025-03-22 12:13:45 -0400

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import BusRouteApp from "@/components/bus-route-app"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <BusRouteApp />
+    </main>
+  )
+}
+
